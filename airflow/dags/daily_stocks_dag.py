22ec2bb7652dd5056ce275e20518ba0d12fb4f07
jpurrutia <jpurrutia95@gmail.com>
2022-03-23 05:36:47 -0500

    edited stock script

--- airflow/dags/daily_stocks_dag.py
+++ airflow/dags/daily_stocks_dag.py
@@ -1,5 +1,10 @@
 import os
 from datetime import datetime
+import pandas as pd
+import yfinance as yf
+import sys
+import math
+import requests
 
 from airflow import DAG
 from airflow.utils.dates import days_ago
@@ -12,6 +17,8 @@ from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExte
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 PROJECT_ID = os.environ.get('PROJECT_ID')
 BUCKET = os.environ.get('GCP_GCS_BUCKET')
+
+MAX_WORKERS = 9
 
 
 default_args = {
@@ -40,6 +47,7 @@ def upload_to_gcs(bucket, object_name, local_file):
     blob.upload_from_filename(local_file)
 
 
+
 with DAG(
     dag_id="data_ingestion_gcs_dag",
     schedule_interval="0 0 * * 1-5",
@@ -52,9 +60,25 @@ with DAG(
 
     download_stocks_list = BashOperator(
         task_id="download_stocks_list",
-        bash_command = "python3 /opt/airflow/dags/py_scripts/get_daily_stocks.py {{ dag_run.logical_date | ds }}",
+        bash_command = "python3 /opt/airflow/dags/py_scripts/get_daily_stocks.py {{ dag_run.logical_date | ds }} {{ next_ds }}",
         dag=dag,
     )
 
+    bucket_ingest_task = PythonOperator(
+        task_id="bucket_ingest",
+        python_callable=upload_to_gcs,
+        op_kwargs={
+            "bucket": BUCKET,
+            "local_file": "/opt/airflow/stocks_{{ dag_run.logical_date | ds }}.csv",
+            "object_name": "raw/stocks_data_{{ dag_run.logical_date | ds }}.csv",
+        },
+    )
 
-    download_stocks_list 
+    rm_task = BashOperator(
+    task_id="rm_task",
+    bash_command="rm /opt/airflow/stocks_{{ dag_run.logical_date | ds }}.csv"
+    
+    )
+
+
+    download_stocks_list >> bucket_ingest_task >> rm_task
--- airflow/dags/py_scripts/get_daily_stocks.py
+++ airflow/dags/py_scripts/get_daily_stocks.py
@@ -7,7 +7,10 @@ import requests
 import io
 
 
-date = sys.argv[1]
+start_date = sys.argv[1]
+end_date = sys.argv[2]
+
+
 
 
 url="https://pkgstore.datahub.io/core/nasdaq-listings/nasdaq-listed_csv/data/7665719fb51081ba0bd834fde71ce822/nasdaq-listed_csv.csv"
@@ -17,17 +20,19 @@ companies = pd.read_csv(io.StringIO(s.decode('utf-8')))
 
 symbols = companies['Symbol'].tolist()
 
+#symbols = ['AAPL','AMZN','GOOGL','MSFT']
+
 stock_final = pd.DataFrame()
 # iterate over each symbol
 
 # iterate over each symbol
-for i in symbols:  
+for i in symbols:
     # print the symbol which is being downloaded
-    print( str(symbols.index(i)) + str(' : ') + i, sep=',', end=',', flush=True)  
+    #print( str(symbols.index(i)) + str(' : ') + i, sep=',', end=',', flush=True)  
     try:
         # download the stock price 
         stock = []
-        stock = yf.download(i,start=date, end=date, progress=False)
+        stock = yf.download(i ,start=start_date, end=end_date, progress=False)
         print(stock)
         # append the individual stock prices 
         if len(stock) == 0:
@@ -37,4 +42,5 @@ for i in symbols:
             stock_final = stock_final.append(stock,sort=False)
     except Exception:
         None
-stock_final.to_csv(f'"/opt/airflow/stocks_{date}.csv')
+
+stock_final.to_csv(f'/opt/airflow/stocks_{start_date}.csv')
